6f898992c71d3a2c6e69dc5320ab18956e4e0f50
TianyiZhang <timothy.tudis@gmail.com>
2020-01-23 10:53:41 -0500

    11

--- Lab-3-unittest-function.js
+++ Lab-3-unittest-function.js
@@ -1,8 +1,13 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE checkHumbrId FUNCTION ==========
-alert("lab 3 Function");//please delete this line once connected.
 /** checkHumbrId Function
  * Validate input as a Humber College Student number.
  * Returns true if input validates.
  * @param {string} idIn
  */
+
+function checkHumbrId(valueIn){
+    let Regex = /n\d{8}/i;
+
+    return Regex.test(valueIn);
+}
